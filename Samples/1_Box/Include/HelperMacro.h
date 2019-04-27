cf797d69ec3a89ba313cfe5a9e010467078bc3fe
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2019-04-27 19:56:02 +0900

    Add Macro

---
+++ Samples/1_Box/Include/HelperMacro.h
@@ -0,0 +1,47 @@
+///
+/// MIT License
+/// Copyright (c) 2019 Jongmin Yun
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+/// @macro Alternative version of DXTrace which removed since Win10 SDK.
+/// This macro does not follow MDY_MACRO name convension because this macro
+/// must substitute original HR WIN32 SDK macro.
+/// @reference http://wergia.tistory.com/6
+#if defined(_WIN32)
+  #if !defined(NDEBUG)
+    #ifndef HR
+      #define HR(__MAExpression__) \
+      { \
+        HRESULT err = (__MAExpression__); \
+        if (err != S_OK) { \
+          LPWSTR errorOutputString; \
+          FormatMessage( \
+              FORMAT_MESSAGE_FROM_SYSTEM | \
+              FORMAT_MESSAGE_IGNORE_INSERTS | \
+              FORMAT_MESSAGE_ALLOCATE_BUFFER, \
+              nullptr, \
+              err, \
+              MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), \
+              (LPTSTR)&errorOutputString, \
+              0, \
+              nullptr); \
+          MessageBoxW(nullptr, errorOutputString, L"Error", MB_OK); \
+        } \
+      }
+    #endif
+  #else
+    #ifndef HR
+      #define HR(__MAExpression__) (__MAExpression__)
+    #endif
+  #endif
+#endif
+
+#define ReleaseCOM(x) { if(x){ x->Release(); x = NULL; } }
