d031dbfef9397e01605a7c9a8ddfdc645aa7372d
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2019-05-05 14:44:09 +0900

    Refactor sample1

--- Samples/1_Box/Source/Entry.cc
+++ Samples/1_Box/Source/Entry.cc
@@ -29,6 +29,8 @@
 #include <XCommonDebug.h>
 #include <IComOwner.h>
 #include <IComBorrow.h>
+#include <FD3D11Factory.h>
+#include <MTimeChecker.h>
 
 #include <StringUtil/XUtility.h>
 
@@ -48,14 +50,17 @@ std::unique_ptr<dy::APlatformBase> platform = nullptr;
 std::optional<dy::DWindowHandle>
 CreateMainWindow(const std::string& titleName, unsigned width, unsigned height)
 {
+  auto value = MTimeChecker::CheckCpuTime("CreateMainWindow");
+
   dy::PWindowCreationDescriptor desc = {};
   desc.mIsWindowFullScreen = false;
   desc.mIsWindowResizable = true;
   desc.mIsWindowShouldFocus = true;
   desc.mIsWindowVisible = true;
-  desc.mWindowName = "D3D11 0_HelloWorld";
+  desc.mWindowName = "D3D11 1_Box";
   desc.mWindowWidth = 800;
   desc.mWindowHeight = 600;
+
   
 #ifdef _WIN32
   #ifdef CreateWindow
@@ -80,137 +85,6 @@ CreateMainWindow(const std::string& titleName, unsigned width, unsigned height)
   return optRes;
 }
 
-/// @brief
-std::optional<
-  std::pair<IComOwner<ID3D11Device>, IComOwner<ID3D11DeviceContext>>
->
-CreateD3D11Device()
-{
-  IComOwner<ID3D11Device>         mD3DDevice            = nullptr;
-  IComOwner<ID3D11DeviceContext>  mD3DImmediateContext  = nullptr;
-  D3D_FEATURE_LEVEL               featureLevel;
-  
-  // Crate D3D11 Device & Context
-  UINT createDeviceFlags = 0;
-#if defined(DEBUG) || defined(_DEBUG)
-  createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
-#endif
-
-  // Make device. (physical? logical?)
-  HRESULT hr = D3D11CreateDevice(
-    nullptr,              // Default Adapter (Primary)
-    D3D_DRIVER_TYPE_HARDWARE, // Use hardware driver (Most optimal) 
-    nullptr,              // No Software device because we use TYPE_HARDWARE (D3D11).
-    createDeviceFlags,    // Set flags (DEBUG, SINGLETHREAD etc...)
-    nullptr,              // If no flag is exist, just pick up the highest version of SDK.
-    0,                    // Above argument brings the array of D3D_FEATURE, so we set it to 0 as nullptr. 
-    D3D11_SDK_VERSION,    // Always specify this.
-    &mD3DDevice,          // Output
-    &featureLevel,        // Output
-    &mD3DImmediateContext // Output
-  );
-  
-  // Error checking.
-  if (FAILED(hr))
-  {
-    platform->GetDebugManager().OnAssertionFailed(
-      "D3D11CreateDevice Failed.\n", __FUNCTION__, __FILE__, __LINE__
-    );
-    return std::nullopt;
-  }
-  if (featureLevel != D3D_FEATURE_LEVEL_11_0)
-  {
-    platform->GetDebugManager().OnAssertionFailed(
-      "Direct3D Feature Level 11 unsupported.\n", __FUNCTION__, __FILE__, __LINE__
-    );
-    return std::nullopt;
-  }
-
-  return std::pair{std::move(mD3DDevice), std::move(mD3DImmediateContext)};
-}
-
-/// @brief OLD Style.
-HRESULT CompileShaderFromFile(
-  const std::string& szFilePath, 
-  LPCSTR szEntryPoint, 
-  LPCSTR szShaderModel, 
-  ID3DBlob** ppBlobOut)
-{
-  DWORD dwShaderFlags = D3DCOMPILE_ENABLE_STRICTNESS;
-#ifdef _DEBUG
-  // Set the D3DCOMPILE_DEBUG flag to embed debug information in the shaders.
-  // Setting this flag improves the shader debugging experience, but still allows 
-  // the shaders to be optimized and to run exactly the way they will run in 
-  // the release configuration of this program.
-  dwShaderFlags |= D3DCOMPILE_DEBUG;
-  // Disable optimizations to further improve shader debugging
-  dwShaderFlags |= D3DCOMPILE_SKIP_OPTIMIZATION;
-#endif
-
-  // Convert file path to wchar_t string.
-  const auto wSzFilePath = dy::ConvertStringToWString(szFilePath);
-
-  std::filesystem::path fsPath = szFilePath;
-  if (std::filesystem::exists(fsPath) == false)
-  {
-    const auto absolutePath = std::filesystem::absolute(fsPath);
-    platform->GetDebugManager().OnAssertionFailed(
-      absolutePath.string().c_str(),
-      __FUNCTION__, __FILE__, __LINE__
-    );
-    return E_FAIL;
-  }
-
-  // Create file descriptor.
-  FILE* fdFile = fopen(szFilePath.c_str(), "r");
-  if (fdFile == nullptr) 
-  { 
-    platform->GetDebugManager().OnAssertionFailed(
-      "Failed to read shader file.", __FUNCTION__, __FILE__, __LINE__
-    );
-    return E_FAIL; 
-  }
-
-  // Read file buffer into fdBuffer.
-  fseek(fdFile, 0, SEEK_END);
-  const auto fdLength = ftell(fdFile);
-  fseek(fdFile, 0, SEEK_SET);
-
-  std::vector<char> fdBuffer(fdLength, 0);
-  fread(fdBuffer.data(), sizeof(char), fdLength, fdFile);
-  assert(feof(fdFile) != 0);
-
-  // Close file descriptor.
-  fclose(fdFile);
-
-  // D3DCompileFromFile.
-  // https://docs.microsoft.com/en-us/windows/desktop/api/d3dcompiler/nf-d3dcompiler-d3dcompilefromfile
-  // https://docs.microsoft.com/ko-kr/windows/desktop/api/d3dcompiler/nf-d3dcompiler-d3dcompile2
-  ID3DBlob* pErrorBlob = nullptr;
-  HRESULT hr = D3DCompile2(
-    fdBuffer.data(), fdBuffer.size(),
-    nullptr,
-    nullptr, nullptr,
-    szEntryPoint, szShaderModel,
-    dwShaderFlags, 0,
-    0,
-    nullptr, 0,
-    ppBlobOut, &pErrorBlob
-  );
-
-  // Check error.
-  if (FAILED(hr))
-  {
-    if (pErrorBlob != nullptr)
-    {
-      OutputDebugStringA(reinterpret_cast<const char*>(pErrorBlob->GetBufferPointer()));
-    }
-  }
-
-  ReleaseCOM(pErrorBlob);
-  return hr;
-}
-
 int WINAPI WinMain(
   [[maybe_unused]] HINSTANCE hInstance, 
   [[maybe_unused]] HINSTANCE hPrevInstance, 
@@ -226,8 +100,11 @@ int WINAPI WinMain(
   auto optRes = CreateMainWindow("D3D11 0_HelloWorld", 800, 600);
   assert(optRes.has_value() == true);
 
+  const auto& checker = MTimeChecker::Get("CreateMainWindow");
+  LOG("[%s] : %.4lf second.\n", "CreateMainWindow", checker.GetRecent().count());
+
   // Crate D3D11 Device & Context
-  auto optDeviceContext = CreateD3D11Device();
+  auto optDeviceContext = FD3D11Factory::CreateD3D11Device(*platform);
   assert(optDeviceContext.has_value() == true);
 
   auto mD3DDevice           = optDeviceContext.value().first.GetBorrow();
@@ -370,7 +247,9 @@ int WINAPI WinMain(
   IComOwner<ID3D11InputLayout> ownVsLayout = nullptr;
   {
     IComOwner<ID3DBlob> ownVsBlob = nullptr;
-    HR(CompileShaderFromFile("../../Resource/Shader.fx", "VS", "vs_5_0", &ownVsBlob));
+    HR(FD3D11Factory::CompileShaderFromFile(
+      *platform,
+      "../../Resource/Shader.fx", "VS", "vs_5_0", &ownVsBlob));
 
     HR(mD3DDevice->CreateVertexShader(
       ownVsBlob->GetBufferPointer(), ownVsBlob->GetBufferSize(), 
@@ -402,7 +281,9 @@ int WINAPI WinMain(
   IComOwner<ID3D11PixelShader> ownPsShader = nullptr;
   {
     IComOwner<ID3DBlob> ownPsBlob = nullptr;
-    HR(CompileShaderFromFile("../../Resource/Shader.fx", "PS", "ps_5_0", &ownPsBlob));
+    HR(FD3D11Factory::CompileShaderFromFile(
+      *platform, 
+      "../../Resource/Shader.fx", "PS", "ps_5_0", &ownPsBlob));
 
     HR(mD3DDevice->CreatePixelShader(
       ownPsBlob->GetBufferPointer(), ownPsBlob->GetBufferSize(), 
@@ -503,6 +384,7 @@ int WINAPI WinMain(
   // Loop
 	while (platform->CanShutdown() == false)
 	{
+    auto _ = MTimeChecker::CheckCpuTime("CpuFrame");
     platform->PollEvents();
 
     // D3D Routine
@@ -539,8 +421,10 @@ int WINAPI WinMain(
       ImGui::Text("counter = %d", counter);
 
       ImGui::Text(
-        "Application average %.3f ms/frame (%.1f FPS)", 
+        "Application average of ImGui %.3f ms/frame (%.1f FPS)", 
         1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+      ImGui::Text("CPU Average frame %.3f ms/frame",
+        MTimeChecker::Get("CpuFrame").GetAverage().count() * 1000.0);
       ImGui::End();
     }
 
